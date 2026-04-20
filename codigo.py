e05553a0be1b573556fd36420839ca8bb2d456de
Brandon Garcia <brandonazael19@gmail.com>
2026-04-19 18:33:20 -0600

    Se modifico el archivo para poner getters y setters

--- codigo.py
+++ codigo.py
@@ -5,3 +5,32 @@ class CuentaBancaria:
         self.__saldo = saldo
         self.__tipo_cuenta = tipo_cuenta
         self.__banco = banco
+    def get_titular(self):
+        return self.__titular
+
+    def get_numero_cuenta(self):
+        return self.__numero_cuenta
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_tipo_cuenta(self):
+        return self.__tipo_cuenta
+
+    def get_banco(self):
+        return self.__banco
+
+    def set_titular(self, titular):
+        self.__titular = titular
+
+    def set_numero_cuenta(self, numero_cuenta):
+        self.__numero_cuenta = numero_cuenta
+
+    def set_saldo(self, saldo):
+        self.__saldo = saldo
+
+    def set_tipo_cuenta(self, tipo_cuenta):
+        self.__tipo_cuenta = tipo_cuenta
+
+    def set_banco(self, banco):
+        self.__banco = banco
