f77d31c76cc2998497de1eaf68f07bcd86874e89
Brandon Garcia <brandonazael19@gmail.com>
2026-04-19 18:24:27 -0600

    Se modifico el archivo con un def __init__

--- codigo.py
+++ codigo.py
@@ -1,2 +1,7 @@
 class CuentaBancaria:
-    pass
+    def _init_(self, titular, numero_cuenta, saldo, tipo_cuenta, banco):
+        self.__titular = titular
+        self.__numero_cuenta = numero_cuenta
+        self.__saldo = saldo
+        self.__tipo_cuenta = tipo_cuenta
+        self.__banco = banco
